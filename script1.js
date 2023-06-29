d3ea76b3e0651200e70984255fd28de34e187df3
kesunn <134431813+kesunn@users.noreply.github.com>
2023-06-29 15:58:41 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,22 +1,17 @@
-// Trying to replicate this:
-// https://simkl.com/movies/1007540/aquaman-and-the-lost-kingdom/countdown/
-const CountToDate = new Date(2023, 6, 9);
-let PreviousTimeBetweenDates;
+const PastDate = new Date(2023, 2, 21, 18); // March 21, 2023
 
-const CountDown = setInterval(() => {
+const CountUp = setInterval(() => {
     const CurrentDate = new Date();
-    const TimeBetweenDates = Math.ceil((CountToDate - CurrentDate)/1000);
-    if (TimeBetweenDates <=0 ) clearInterval(CountDown);
-    FlipAllCards(TimeBetweenDates);
-    PreviousTimeBetweenDates = TimeBetweenDates;
+    const TimePassed = Math.floor((CurrentDate - PastDate) / 1000);
+    FlipAllCards(TimePassed);
 }, 250);
 
-function FlipAllCards(time){
+function FlipAllCards(time) {
     const secs = time % 60;
-    const mins = Math.floor(time /60) % 60;
+    const mins = Math.floor(time / 60) % 60;
     const hrs = Math.floor(time / 3600) % 24;
     const days = Math.floor(time / 86400);
-    console.log(days, hrs, mins,secs);
+    console.log(days, hrs, mins, secs);
     Flipper(document.querySelector("[data-seconds-ones]"), secs % 10);
     setTimeout(() => {
         Flipper(document.querySelector("[data-seconds-tens]"), Math.floor(secs / 10));
@@ -30,31 +25,31 @@ function FlipAllCards(time){
 }
 
 function Flipper(FlipCard, newNumber) {
-  const TopHalf = FlipCard.querySelector(".flip-top");
-  const StartNumber = parseInt(TopHalf.textContent);
-  
-  if(newNumber === StartNumber) return;
+    const TopHalf = FlipCard.querySelector(".flip-top");
+    const StartNumber = parseInt(TopHalf.textContent);
 
-  const BottomHalf = FlipCard.querySelector(".flip-bottom");
-  const TopFlip = document.createElement("div");
-  TopFlip.classList.add("top-flip");
-  const BottomFlip = document.createElement("div");
-  BottomFlip.classList.add("bottom-flip");
+    if (newNumber === StartNumber) return;
 
-  TopHalf.textContent = StartNumber;
-  TopFlip.textContent = StartNumber;
-  BottomHalf.textContent = StartNumber;
-  BottomFlip.textContent = newNumber;
+    const BottomHalf = FlipCard.querySelector(".flip-bottom");
+    const TopFlip = document.createElement("div");
+    TopFlip.classList.add("top-flip");
+    const BottomFlip = document.createElement("div");
+    BottomFlip.classList.add("bottom-flip");
 
-  TopFlip.addEventListener("animationstart", (e) => {
-    TopHalf.textContent = newNumber;
-  });
-  TopFlip.addEventListener("animationend", (e) => {
-    TopFlip.remove();
-});
-BottomFlip.addEventListener("animationend", (e) => {
-    BottomHalf.textContent = newNumber;
-    BottomFlip.remove();
-  });
-  FlipCard.append(TopFlip, BottomFlip);
+    TopHalf.textContent = StartNumber;
+    TopFlip.textContent = StartNumber;
+    BottomHalf.textContent = StartNumber;
+    BottomFlip.textContent = newNumber;
+
+    TopFlip.addEventListener("animationstart", (e) => {
+        TopHalf.textContent = newNumber;
+    });
+    TopFlip.addEventListener("animationend", (e) => {
+        TopFlip.remove();
+    });
+    BottomFlip.addEventListener("animationend", (e) => {
+        BottomHalf.textContent = newNumber;
+        BottomFlip.remove();
+    });
+    FlipCard.append(TopFlip, BottomFlip);
 }
